091524ec9df0f3a156b58a805f305df36c8453f7
jatin510 <jatin6972@gmail.com>
2025-06-04 12:28:15 +0530

    send + sync

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,43 @@
 // we have to download 100k urls
-use std::thread::available_parallelism;
 use reqwest::Client;
+use std::thread::available_parallelism;
 use tokio::runtime::Builder;
 use tokio::task;
 // use reqwest::
 
+mod send_sync;
+
+use send_sync::run_send_sync;
+
 #[tokio::main]
 async fn main() {
+    run_send_sync();
 
-    // let value = "https://google.com";
-    // let count = 100;
-    // let urls = std::iter::repeat(value).take(count).collect();
-    let urls = vec!["https://google.com";10000];
-    // println!("urls.length {:?}", urls.len());
-    // println!("urls {}", urls.get(1).unwrap());
-
-    // let urls = vec!["https://google.com";100];
-    let mut join_handlers =Vec::new();
-
-    let client = reqwest::Client::new();
-
-
-    for url in urls {
-        let client  =client.clone();
-        let join = task::spawn(async  move{
-            process_url(url, client.clone()).await
-        });
-
-      join_handlers.push(join);
-    }
-
-    for join_handler in join_handlers{
-        join_handler.await.unwrap();
-    }
-
+    // // let value = "https://google.com";
+    // // let count = 100;
+    // // let urls = std::iter::repeat(value).take(count).collect();
+    // let urls = vec!["https://google.com"; 100];
+    // // println!("urls.length {:?}", urls.len());
+    // // println!("urls {}", urls.get(1).unwrap());
+    //
+    // // let urls = vec!["https://google.com";100];
+    // let mut join_handlers = Vec::new();
+    //
+    // let client = reqwest::Client::new();
+    //
+    // for url in urls {
+    //     let client = client.clone();
+    //     let join = task::spawn(async move { process_url(url, client.clone()).await });
+    //
+    //     join_handlers.push(join);
+    // }
+    //
+    // for join_handler in join_handlers {
+    //     join_handler.await.unwrap();
+    // }
 }
 
-async fn process_url(url:&str, client: Client) {
+async fn process_url(url: &str, client: Client) {
     println!("process url {:?}", url);
     let resp = client.get(url).send().await.unwrap();
 
---
+++ src/send_sync.rs
@@ -0,0 +1,55 @@
+use std::sync::Arc;
+use std::thread;
+
+trait Vehicle: Send + Sync {
+    fn start(&self);
+}
+
+struct Car {}
+
+impl Car {
+    fn new() -> Car {
+        Self {}
+    }
+}
+struct Bike {}
+impl Bike {
+    fn new() -> Bike {
+        Self {}
+    }
+}
+impl Vehicle for Car {
+    fn start(&self) {
+        println!("starting car");
+    }
+}
+
+impl Vehicle for Bike {
+    fn start(&self) {
+        println!("starting bike");
+    }
+}
+
+pub fn run_send_sync() {
+    let vehicles: Vec<Box<dyn Vehicle>> = vec![Box::new(Car::new()), Box::new(Bike::new())];
+
+    vehicles.into_iter().for_each(|vehicle| {
+        thread::spawn(move || vehicle.start())
+            .join()
+            .expect("Failed to start the vehicle")
+    });
+
+    let car: Arc<dyn Vehicle> = Arc::new(Car::new());
+
+    let handles: Vec<_> = (0..10).map(|_| {
+        let car_shared = Arc::clone(&car);
+
+        thread::spawn(move || {
+            car_shared.start();
+        })
+    }).collect();
+
+    for handle in handles {
+        handle.join().expect("failed");
+    }
+}
